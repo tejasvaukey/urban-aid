09a7433f0e560888bf264794d0dfd133c9641171
Tejasva Ukey <tejasvaukey123@gmail.com>
2024-10-23 00:31:21 +0530

    Add styles

--- Frontend/src/Components/HomePage.jsx
+++ Frontend/src/Components/HomePage.jsx
@@ -1,1 +1,10 @@
 
+// import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      This is home page
+    </div>
+  );
+}
---
+++ Frontend/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './navbar.css'
+
+export default function Navbar() {
+  return (
+    <>
+        <h1 className="bg-red">
+      This is navbarrrrr
+        </h1>
+            
+    </>
+  );
+}
